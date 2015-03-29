faa08b0b4fe17a0f92012dd185107cfab6df19c8
Jon Long <jon@jon-long.ca>
2015-03-30 05:30:13 +0900

    Get web site teaser display to work

--- jl.php
+++ jl.php
@@ -10,4 +10,38 @@ function predump($string) {
 	?></pre><?php
 }
 
+function d($string) {
+	predump($string);
+}
+
+function map($collection, $callback) {
+	$mapped = [];
+	foreach($collection as $item) {
+		array_push($mapped, call_user_func($callback, $item));
+	}
+	return $mapped;
+}
+
+/**
+ * WordPress specific functions
+ */
+
+function get_posts_from_category($term_id, $post_type = 'post', $taxonomy = 'category' ) {
+	$args = array(
+		'post_type' => $post_type,
+		'tax_query' => array(
+			array(
+				'taxonomy' => $taxonomy,
+				'field'    => 'id',
+				'terms'    => $term_id, 	
+			)
+		)
+	);
+	return get_posts( $args );
+}
+
+function get_post_thumbnail_url($post_id) {
+	return wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ))[0];
+}
+
 use \jl;
--- plugin/controllers/WebGuideController.php
+++ plugin/controllers/WebGuideController.php
@@ -1,5 +1,8 @@
 <?php
 
+
+
+
 class WebGuideController extends BaseController {
 
 	/**
@@ -7,27 +10,52 @@ class WebGuideController extends BaseController {
 	*/
 	public function index()
 	{
+
+
 		// I will first get all the categories
-		// And for each cateogry I will display it with a
-
-		// and pass it to a view partial.
-
-		// I will loop over the top web site categories
 
 		function get_top_website_categories() {
-			jl\predump(get_categories(array(
+			return get_categories(array(
 				'taxonomy' => 'website_category',
-			)));
+			));
 		}
 
-		get_top_website_categories();
+		$categories = get_top_website_categories();
+
+
+		// And for each cateogry I will display it with a view
+
+		$html_to_return = '';
+		foreach ( $categories as $category ) {
+			$website_posts =
+				jl\get_posts_from_category( 2, 'website', 'website_category' );
+			
+			// jl\d($website_posts);
+
+			$websites_in_category = jl\map($website_posts, function($website_post) {
+				$website = new stdClass();
+				$website->title = $website_post->post_title;
+				$website->description = $website_post->post_excerpt;
+				$website->url = get_post_meta($website_post->ID, 'url', true);
+				$website->thumbnail = jl\get_post_thumbnail_url( $website_post->ID );
+				return $website;
+			});
+
+			$html_to_return .= $this->view->render( 'category-teaser', [
+				'category_title' => $category->name,
+				'category_id' => $category->term_id,
+				'websites' => $websites_in_category,
+				] );
+		}
 
 		// I for each category I will list all the websites
+
+		return $html_to_return;
+
+
 
 		// If there are sub categories I will loop over any subcategories
 
 		// And then list all the websites under there
-		$categories = [];
-		return $this->view->render( 'index', ['categories' => $categories] );
 	}
 }
